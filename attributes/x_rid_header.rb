fa504e72ec50fdb518917d25627f070c033bc15c
Nick Muerdter <nick.muerdter@nrel.gov>
2013-07-28 23:53:19 -0600

    Add recipe for installing nginx-x-rid-module (for unique request ids).

---
+++ attributes/x_rid_header.rb
@@ -0,0 +1,3 @@
+default['nginx']['x_rid_header']['version'] = '0daa3cc283d91a279a6013734fd78264582fce51'
+default['nginx']['x_rid_header']['source_url'] = "https://github.com/gabor/nginx-x-rid-header/archive/#{node['nginx']['x_rid_header']['version']}.tar.gz"
+default['nginx']['x_rid_header']['source_checksum'] = '29b01140590515327f777ea3228a673a723d877e1a5fd040fc7307a1887e8e6f'
