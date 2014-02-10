120b20040328a25810656c39d61499cb7d2db4d3
Nick Muerdter <nick.muerdter@nrel.gov>
2014-02-10 14:02:36 -0700

    Revert "[COOK-3923] - Enable the list of packages installed by nginx::passenger to be configurable"
    This reverts commit f42cdc24bba70bb84ca40cf17ba871efd3ac596e.

--- recipes/passenger.rb
+++ recipes/passenger.rb
@@ -27,14 +27,12 @@ if node["languages"].attribute?("ruby")
 end
 
 packages = value_for_platform_family(
-  %w[rhel]   => node['nginx']['passenger']['packages']['rhel'],
-  %w[debian] => node['nginx']['passenger']['packages']['debian']
+  %w[rhel]   => %w[ruby-devel curl-devel],
+  %w[debian] => %w[ruby-dev libcurl4-gnutls-dev]
 )
 
-unless packages.empty?
-  packages.each do |name|
-    package name
-  end
+packages.each do |name|
+  package name
 end
 
 if(!node['rbenv']['global'] || node['rbenv']['global'] < "2.0.0")
